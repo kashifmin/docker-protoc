4e79d47308063da3c83f0d801720d2ba08ffbd6a
Kashif Minhaj <dailypctips@gmail.com>
2020-06-18 10:47:00 +0530

    Remove grpc java

--- all/install-protobuf.sh
+++ all/install-protobuf.sh
@@ -5,10 +5,10 @@ if [ -z $1 ]; then
     exit 1
 fi
 
-if [ -z $2 ]; then
-    echo "You must specify a grpc-java version."
-    exit 1
-fi
+# if [ -z $2 ]; then
+#     echo "You must specify a grpc-java version."
+#     exit 1
+# fi
 
 curl -sSL https://github.com/gflags/gflags/archive/v2.2.1.tar.gz -o gflags.tar.gz && tar -xzvf gflags.tar.gz
 cd gflags-2.2.1/
@@ -22,9 +22,6 @@ git clone -b v$1.x --recursive -j8 https://github.com/grpc/grpc
 cd /tmp/grpc
 CFLAGS="-Wno-cast-function-type" make
 make install
-# php support
-git submodule update --init
-make grpc_php_plugin
 
 cp /tmp/grpc/bins/opt/protobuf/protoc /usr/local/bin/
 
@@ -37,7 +34,3 @@ cd /tmp/grpc/third_party/protobuf
 cd /tmp/grpc
 make grpc_cli
 
-cd /tmp
-git clone -b v$2.x --recursive https://github.com/grpc/grpc-java.git
-cd /tmp/grpc-java/compiler
-../gradlew -PskipAndroid=true java_pluginExecutable
